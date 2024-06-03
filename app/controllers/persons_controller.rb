dca3367c690c25e9facfd51b0fc6c362198c1e12
JaValderaContador <javier1gdvc@gmail.com>
2024-06-03 09:55:12 -0400

    V1.0

---
+++ app/controllers/persons_controller.rb
@@ -0,0 +1,10 @@
+class PersonsController < ApplicationController
+  def index
+    @people = Person.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @people }
+    end
+  end
+end
